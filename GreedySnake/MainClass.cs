e52203fd6cd03e62bce464c516fc7a5a7a6f0335
DESKTOP-OSHLUMQ\yao <busiyg@163.com>
2018-04-25 12:57:49 +0800

    Built Project

---
+++ GreedySnake/MainClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedySnake {
+    class MainClass {
+        static void Main() {
+            string strname = Console.ReadLine();
+            Console.WriteLine(strname);
+            Console.ReadKey();
+
+
+        }
+    }
+}
